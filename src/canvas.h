1b9da47085625d337a395bda431327cab57c1fa7
Naphtha Nepanthez <naphtha@lotte.link>
2020-08-25 00:29:43 +0100

    Accidental semicolon

--- src/canvas.h
+++ src/canvas.h
@@ -137,7 +137,7 @@
     void SR_DestroyCanvas(SR_Canvas *canvas);
 
     // Check if the canvas has been successfully allocated
-    #define SR_CanvasIsValid(canvas) (BOOLIFY((canvas)->pixels));
+    #define SR_CanvasIsValid(canvas) (BOOLIFY((canvas)->pixels))
 
     /* Malloc a new canvas of given size and start copying every pixel from the
      * specified old canvas to the new one, starting at the given position.
