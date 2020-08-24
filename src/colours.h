b126540a9531da2f3fac19d014c4c6898f804bd8
Naphtha Nepanthez <naphtha@lotte.link>
2020-08-24 23:22:47 +0100

    Always inline colour functions

--- src/colours.h
+++ src/colours.h
@@ -36,7 +36,7 @@
     };
 
     // Construct an RGB colour value.
-    __inline__ SR_RGBPixel SR_CreateRGB(
+    inline __attribute__((always_inline)) SR_RGBPixel SR_CreateRGB(
         register uint8_t red,
         register uint8_t green,
         register uint8_t blue)
@@ -45,7 +45,7 @@
     }
 
     // Create an RGBA colour value.
-    __inline__ SR_RGBAPixel SR_CreateRGBA(
+    inline __attribute__((always_inline)) SR_RGBAPixel SR_CreateRGBA(
         register uint8_t red,
         register uint8_t green,
         register uint8_t blue,
@@ -58,7 +58,9 @@
     // Conversion
     #define SR_RGBAtoRGB(pixel) (pixel.rgb)
 
-    __inline__ SR_RGBAPixel SR_RGBtoRGBA(SR_RGBPixel pixel, uint8_t alpha)
+    inline __attribute__((always_inline)) SR_RGBAPixel SR_RGBtoRGBA(
+        SR_RGBPixel pixel,
+        uint8_t alpha)
     {
         SR_RGBAPixel temp;
         temp.rgb = pixel;
@@ -67,11 +69,11 @@
     }
 
     #ifdef SURCL_ALLOW_TYPE_PUNNING
-        __inline__ uint32_t SR_RGBAtoWhole(SR_RGBAPixel pix)
-            { return *(uint32_t *) &pix; }
+        inline __attribute__((always_inline)) uint32_t SR_RGBAtoWhole(
+            SR_RGBAPixel pix) { return *(uint32_t *) &pix; }
 
-        __inline__ SR_RGBAPixel SR_WholetoRGBA(uint32_t pix)
-            { return *(SR_RGBAPixel *) &pix; }
+        inline __attribute__((always_inline)) SR_RGBAPixel SR_WholetoRGBA(
+            uint32_t pix) { return *(SR_RGBAPixel *) &pix; }
     #else
         #define SR_RGBAtoWhole(pix) (  \
             ((pix).rgb.red        ) |  \
@@ -80,7 +82,8 @@
             ((pix).alpha     << 24)    \
         )
 
-        __inline__ SR_RGBAPixel SR_WholetoRGBA(uint32_t pix)
+        inline __attribute__((always_inline)) SR_RGBAPixel SR_WholetoRGBA(
+            uint32_t pix)
         {
             SR_RGBAPixel temp;
             temp.rgb.red   = (pix & 0x000000FF)      ;
@@ -94,9 +97,86 @@
     // Blend RGBA values
     // Use mode provided by SR_BlendingModes
     // Usually, you'll want to set alpha_modifier to 255.
-    SR_RGBAPixel SR_RGBABlender(
+    inline __attribute__((always_inline)) SR_RGBAPixel SR_RGBABlender(
         SR_RGBAPixel pixel_base,
         SR_RGBAPixel pixel_top,
         register uint8_t alpha_modifier,
-        register char mode);
+        register char mode)
+    {
+        // TODO: Make this faster and cleaner. Much faster and cleaner!
+
+        register uint32_t final, pixel_base_whole, pixel_top_whole = 0;
+        register float alpha_mul, alpha_mul_neg;
+
+        if (mode != SR_BLEND_ADDITIVE && mode != SR_BLEND_XOR) goto srbl_nomul;
+
+        alpha_mul = (
+            ((float)pixel_top.alpha / 255.0) *
+            ((float)alpha_modifier / 255.0)
+        );
+        alpha_mul_neg = 1 - alpha_mul;
+
+        pixel_top.rgb.red   *= alpha_mul;
+        pixel_top.rgb.blue  *= alpha_mul;
+        pixel_top.rgb.green *= alpha_mul;
+
+        pixel_base.rgb.red   *= alpha_mul_neg;
+        pixel_base.rgb.blue  *= alpha_mul_neg;
+        pixel_base.rgb.green *= alpha_mul_neg;
+
+    srbl_nomul:
+        pixel_base_whole = SR_RGBAtoWhole(pixel_base);
+        pixel_top_whole  = SR_RGBAtoWhole(pixel_top );
+
+        switch (mode) {
+        case SR_BLEND_DIRECT_XOR: // Mul skipped by goto srbl_nomul
+        case SR_BLEND_XOR: // Mul version
+            final = pixel_base_whole ^ (pixel_top_whole & 0x00FFFFFF);
+
+            break;
+        case SR_BLEND_ADDITIVE:
+            final = (
+                (MIN(((pixel_base_whole & 0x000000FF)      ) +
+                ((pixel_top_whole & 0x000000FF)      ), 255)      ) |
+                (MIN(((pixel_base_whole & 0x0000FF00) >> 8 ) +
+                ((pixel_top_whole & 0x0000FF00) >> 8 ), 255) << 8 ) |
+                (MIN(((pixel_base_whole & 0x00FF0000) >> 16) +
+                ((pixel_top_whole & 0x00FF0000) >> 16), 255) << 16) |
+                (pixel_base_whole & 0xFF000000)
+            );
+
+            break;
+        case SR_BLEND_OVERLAY:
+            if (((uint16_t)alpha_modifier * pixel_top.alpha) > 16129)
+                final = ((pixel_top_whole & 0x00FFFFFF) |
+                    (pixel_base_whole & 0xFF000000));
+            else final = pixel_base_whole;
+
+            break;
+        case SR_BLEND_INVERT_DROP:
+            final  = (pixel_base_whole & 0x00FFFFFF);
+            final |= (255 - ((pixel_top_whole & 0xFF000000) >> 24)) << 24;
+
+            break;
+        case SR_BLEND_DROP:
+            final = ((pixel_base_whole & 0x00FFFFFF) |
+                (pixel_top_whole & 0xFF000000));
+
+            break;
+        case SR_BLEND_REPLACE:
+            final = pixel_top_whole;
+
+            break;
+        case SR_BLEND_DIRECT_XOR_ALL:
+            final = pixel_base_whole ^ pixel_top_whole;
+            break;
+        default:
+            fprintf(stderr, "Invalid blending mode!\n");
+            exit(EXIT_FAILURE);
+
+            break;
+        }
+
+        return SR_WholetoRGBA(final);
+    }
 #endif
