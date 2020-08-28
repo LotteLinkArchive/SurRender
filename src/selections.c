f74e5a1a31831a9c201861f41e02a37d7ab9c968
marbles-box <h@web.site>
2020-08-28 19:13:33 -0400

    the bare bones, not sure if i did it right

--- src/selections.c
+++ src/selections.c
@@ -3,3 +3,20 @@
 #include "colours.h"
 #include "selections.h"
 
+SR_Select SR_NewSelect(unsigned short width, unsigned short height)
+{
+    SR_Select temp = {};
+    
+    temp.width = width;
+    temp.height = height;
+    
+    temp.bitfield = calloc((width * height) >> 3, sizeof(uint8_t));
+    
+    return temp;
+}
+
+void SR_DestroySelect(SR_Select *selection)
+{
+    free(selection->bitfield);
+    selection->bitfield = NULL;
+}
--- src/selections.h
+++ src/selections.h
@@ -4,5 +4,18 @@
     #include "canvas.h"
     #include "colours.h"
     
+    typedef struct SR_Select {
+        // Width and height, in bits
+        unsigned short width;
+        unsigned short height;
+        
+        // Pointer to an array of bytes
+        uint8_t *bitfield;
+    } SR_Select;
     
+    // Create a new selecty box of given dimensions
+    SR_Select SR_NewSelect(unsigned short width, unsigned short height);
+    
+    // Destroy + free a selecty box
+    void SR_DestroySelect(SR_Select *selection);
 #endif
