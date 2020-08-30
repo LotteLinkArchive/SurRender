b26fb35a7ec4317ee34b900920590e45e67fe431
marbles-box <h@web.site>
2020-08-29 20:38:09 -0400

    ZA HANDO

--- src/selections.c
+++ src/selections.c
@@ -20,21 +20,3 @@ void SR_DestroySelect(SR_Select *selection)
     free(selection->bitfield);
     selection->bitfield = NULL;
 }
-
-void SR_EatBreakfast(
-    SR_Canvas *canvas,
-    SR_Select *selection,
-    unsigned short offset_x,
-    unsigned short offset_y)
-{
-    unsigned short max_x = MIN(canvas->width, selection->width + offset_x);
-    unsigned short max_y = MIN(canvas->height, selection->height + offset_y);
-    unsigned short x, y;
-    for (x = offset_x; x < max_x; x++)
-    for (y = offset_y; y < max_y; y++) {
-        if (SR_SelectGetPoint(selection, x, y) {
-            SR_RGBAPixel rand_col = SR_CreateRGBA(rand(), rand(), rand(), 255);
-            SR_CanvasSetPixel(canvas, x, y, rand_col);
-        }
-    }
-}
