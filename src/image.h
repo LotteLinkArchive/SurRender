043729000d31d3faa0140a23dabe0636df415a89
Naphtha Nepanthez <naphtha@lotte.link>
2020-09-19 23:02:06 +0100

    Corrected documentation for image.h

--- src/image.h
+++ src/image.h
@@ -7,7 +7,7 @@
     // Load a Radix memory blob as an image and convert it into a canvas
     SR_Canvas SR_ImageMemToCanvas(X0 *data, SX length);
 
-    // Load an image file into a new canvas (currently only supports BMP)
+    // Load an image file into a new canvas (Uses stb_image.h)
     SR_Canvas SR_ImageFileToCanvas(CHR *filename);
 
     // WARNING: All created canvases must be freed as they are mallocated.
