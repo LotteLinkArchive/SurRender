0fbcf4071b553d4460569588b025aa03aed32d88
Naphtha Nepanthez <naphtha@lotte.link>
2020-09-20 03:26:43 +0100

    Fixed header tabs

--- src/surrender.h
+++ src/surrender.h
@@ -1,11 +1,11 @@
 #ifndef SURMN_HEADER_FILE
 #define SURMN_HEADER_FILE
-	#include "glbl.h"
-	#include "colours.h"
-	#include "canvas.h"
-	#include "shapes.h"
-	#include "image.h"
-	#include "2dscene.h"
-	#include "selections.h"
-	#include "textrender.h"
+#include "glbl.h"
+#include "colours.h"
+#include "canvas.h"
+#include "shapes.h"
+#include "image.h"
+#include "2dscene.h"
+#include "selections.h"
+#include "textrender.h"
 #endif
