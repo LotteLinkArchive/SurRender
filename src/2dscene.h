0e4ddb03e309e938ebf689b7bc86f61a34da3ef6
Naphtha Nepanthez <naphtha@lotte.link>
2020-10-03 21:03:29 +0100

    SR_2DObject basics

--- src/2dscene.h
+++ src/2dscene.h
@@ -1,4 +1,23 @@
 #ifndef SUR2D_HEADER_FILE
 #define SUR2D_HEADER_FILE
 #include "glbl.h"
+#include "canvas.h"
+
+typedef struct {
+	/* Mandatory details (identity tag, type specification, user data, draw position)
+	 * `tag` and `type` are 32-bit hashes.
+	 */
+	U32    tag;
+	U32    type;
+	X0    *udata;
+	R32x3  pos;
+
+	/* Pointer to an array of pointers to canvases. Each index in the array leads to a pointer to a frame to use
+	 * for animating the 2D object. Index 0 is a pointer to the pointer for the first frame.
+	 */
+	SR_Canvas **sprites;
+
+	/* Index in the sprite canvas array to use (0 for static sprites) */
+	SX drwindex;
+} SR_2DObject;
 #endif
