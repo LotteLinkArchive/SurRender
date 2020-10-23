c27d9cef4e2d082d52c2a2c926b75aae4908b8bf
Naphtha Nepanthez <naphtha@lotte.link>
2020-10-24 00:45:41 +0100

    Documentation of srtex.h

--- src/srtex.h
+++ src/srtex.h
@@ -3,14 +3,19 @@
 
 /* Defines the structure of an SRT texture header. */
 struct SRTHeader {
+	/* The width and height of the SRT texture */
 	I32 width;
 	I32 height;
-	I32 stbi_type;
-	I32 data_length;
-};
 
-/* After the header is written to the file, the entire rest of the file should be of size data_length and contain the
- * required image data.
- */
+	/* The amount of byte-large channels (as returned by STBI) */
+	I32 stbi_type;
+
+	/* The data length in bytes */
+	I32 data_length;
+
+	/* After the header is written to the file, the entire rest of the file should be of size data_length and
+	 * contain the required image data.
+	 */
+};
 
 #endif
