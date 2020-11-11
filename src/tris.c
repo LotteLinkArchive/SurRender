13d518453facbda749936c97991f333afe8606f8
marbles-box <h@web.site>
2020-11-10 19:26:59 -0500

    Supah slow triangle raster

--- src/tris.c
+++ src/tris.c
@@ -3,6 +3,37 @@
 #include "colours.h"
 #include "tris.h"
 
+X0 Trifill_Slow(
+	SR_Canvas *canvas,
+	SR_ScreenTriangle tri)
+{
+	U16 max_x = MAX(tri.v0.x, MAX(tri.v1.x, tri.v2.x));
+	U16 min_x = MIN(tri.v0.x, MAX(tri.v1.x, tri.v2.x));
+	U16 max_y = MAX(tri.v0.y, MAX(tri.v1.y, tri.v2.y));
+	U16 min_y = MIN(tri.v0.y, MAX(tri.v1.y, tri.v2.y));
+	
+	U16 vs1x = tri.v1.x - tri.v0.x;
+	U16 vs1y = tri.v1.y - tri.v0.y;
+	U16 vs2x = tri.v2.x - tri.v0.x;
+	U16 vs2y = tri.v2.y - tri.v0.y;
+	
+	U16 xx, yy;
+	for (xx = min_x, x <= max_x, x++)
+	for (yy = min_y, y <= min_y, y++)
+	{
+		U16 qx = xx - tri.v0.x;
+		U16 qy = yy - tri.v0.y;
+		
+		float c = (float)(vs1x * vs2y - vs1y * vs2x);
+		float s = (float)(qx * vs2y - qy * vs2x) / c;
+		float t = (float)(vs1x * qy - vs1y * qx) / c;
+		
+		if ((s>=0) && (t>=0) && ((s+t)<=1)) {
+			SR_CanvasSetPixel(canvas, xx, yy, tri.colour);
+		}
+	}
+}
+
 X0 SR_RenderTris(
 	SR_Canvas *canvas,
 	SR_ScreenTriangle *trilist,
--- src/tris.h
+++ src/tris.h
@@ -2,20 +2,22 @@
 #include "canvas.h"
 #include "colours.h"
 
-typedef union {
-	struct {
-		U16 x;
-		U16 y;
-		U32 z;
-	} cmp; // "cmp" is shorthand for "components"
-	U64 whole;
+typedef struct {
+	U16 x;
+	U16 y;
+	U32 z;
 } SR_ScreenVertex;
 
 typedef struct {
 	SR_ScreenVertex v0;
 	SR_ScreenVertex v1;
 	SR_ScreenVertex v2;
+	SR_RGBAPixel colour;
 } SR_ScreenTriangle;
+
+X0 Trifill_Slow(
+	SR_Canvas *canvas,
+	SR_ScreenTriangle tri);
 
 X0 SR_RenderTris(
 	SR_Canvas *canvas,
