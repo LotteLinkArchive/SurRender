3a33b6488bf469adae2bdf85cb2c02a3dff2dbe0
Naphtha Nepanthez <naphtha@lotte.link>
2020-11-15 16:59:46 +0000

    Improved devbuild.sh

--- devbuild.sh
+++ devbuild.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-CC=clang meson bin --reconfigure
-cd bin
-meson configure -Dc_args="-Ofast -march=native -mtune=native -g"
-cd ..
+if [ ! -d "./bin" ]; then
+	CC=clang meson bin
+	cd bin
+	meson configure -Dc_args="-Ofast -march=native -mtune=native -g"
+	cd ..
+fi
 meson compile -C bin
