a5a5d5b93b4b3dc9c52006259138818dcac8a087
Naphtha Nepanthez <naphtha@lotte.link>
2020-11-20 02:58:45 +0000

    Remove C++ style comments

--- src/tris.h
+++ src/tris.h
@@ -4,20 +4,20 @@
 #include "canvas.h"
 #include "colours.h"
 
-// screen vertex struct
+/* screen vertex struct */
 typedef struct {
 	U16 x;
 	U16 y;
 	U32 z;
 } SR_ScreenVertex;
 
-// screen triangle struct
+/* screen triangle struct */
 typedef struct {
 	SR_ScreenVertex vx[3];
 	SR_RGBAPixel colour;
 } SR_ScreenTriangle;
 
-// iterates over an array of tris and draws them all
+/* iterates over an array of tris and draws them all */
 X0 SR_RenderTris(
 	SR_Canvas *canvas,
 	SR_ScreenTriangle *trilist,
